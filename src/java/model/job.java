bf571983aaae044d12260af5d8d3cde6fc3329ca
Sravanoruganti <30496455+Sravanoruganti@users.noreply.github.com>
2017-08-31 16:51:44 +0530

    job.java

---
+++ src/java/model/job.java
@@ -0,0 +1,54 @@
+
+package model;
+
+import org.apache.solr.client.solrj.beans.Field;
+
+public class Jobs {
+	
+	private String id;
+	private String jobtitle;
+	private String education;
+	private int experience;
+	private int salary;
+	
+	
+	
+	public String getId() {
+		return id;
+	}
+	@Field
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getJobtitle() {
+		return jobtitle;
+	}
+	@Field
+	public void setJobtitle(String jobtitle) {
+		this.jobtitle = jobtitle;
+	}
+	public String getEducation() {
+		return education;
+	}
+	@Field
+	public void setEducation(String education) {
+		this.education = education;
+	}
+	public int getExperience() {
+		return experience;
+	}
+	@Field
+	public void setExperience(int experience) {
+		this.experience = experience;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	@Field
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+
+}
